3b0ce0bbd43f58a644adb8fd607b9a3f6775b310
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-13 16:07:39 +0100

    solo prompt

---
+++ script/snack-1.js
@@ -0,0 +1,11 @@
+//10 domande e 10 dati
+
+let userNumber;
+for( i = 0 ; i < 10 ; i++){
+    userNumber = parseInt(prompt(" scegli un numero da 1 a 10 " , "1"))
+
+    
+    
+    console.log(result);
+}
+
